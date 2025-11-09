a469116ea02acbcd79813e4fa5f85dc2a3097671
Michael Shingo Crawford <mcrawford5376@gmail.com>
2025-11-09 20:35:49 +0900

    add heroui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,23 @@
 import type { Metadata } from "next";
-import { Noto_Sans_KR } from 'next/font/google';
+import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
 import NextTopLoader from "nextjs-toploader";
 import { Toaster } from "@/components/ui/sonner";
+import { Providers } from "./providers";
 
 const notoSansKR = Noto_Sans_KR({
-  weight: ['300', '400', '500', '700'],
-  subsets: ['latin'],
-  variable: '--font-noto-sans-kr',
-  display: 'swap',
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  variable: "--font-noto-sans-kr",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
   title: "Better Auth Starter",
   description: "Next.js + Better Auth + Shadcn UI + Tailwind CSS",
   icons: {
-    icon: '/logo.png'
-  }
+    icon: "/logo.png",
+  },
 };
 
 export default function RootLayout({
@@ -25,14 +26,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${notoSansKR.variable}`} suppressHydrationWarning>
-      <body
-        className={` antialiased`}
-      >
+    <html
+      lang="en"
+      className={`${notoSansKR.variable}`}
+      suppressHydrationWarning
+    >
+      <body className={` antialiased`}>
         <NextTopLoader showSpinner={false} height={6} color="#000000" />
         <Toaster richColors position="top-right" />
         <main className="min-h-screen">
-          {children}
+          <Providers>{children}</Providers>
         </main>
       </body>
     </html>
